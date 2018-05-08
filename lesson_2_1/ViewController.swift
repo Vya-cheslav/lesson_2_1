1decf214053ac83505af6cf9dd471f460fa77e2f
Вячеслав Рожнов <vaceslavroznov@vaceslavs-mbp.beeline tc-100>
2018-05-08 12:51:23 +0300

    Initial Commit

---
+++ lesson_2_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson_2_1
+//
+//  Created by Вячеслав Рожнов on 08.05.18.
+//  Copyright © 2018 LLC soft-upgrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
