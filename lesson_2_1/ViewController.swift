6c710f73a91e7f7d9788d225083799187391a5ad
Вячеслав Рожнов <vaceslavroznov@vaceslavs-macbook-pro.local>
2018-05-10 09:36:42 +0300

    lesson12

--- lesson_2_1/ViewController.swift
+++ lesson_2_1/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
 
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action:
+            #selector(self.hideKeyboard))
+        // присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +40,38 @@ class ViewController: UIViewController {
 
     }
     
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+
+    // когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        // получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0)
+        // добавляем отступ внизу UIScrollView равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    //когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // устанавливаем отступ внизу UIScrollView равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Подписываемся на два уведомления, одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name:
+            NSNotification.Name.UIKeyboardWillShow, object: nil)
+        // Второе когда она пропадает
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(self.keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide,
+                                                                 object: nil)
+    }
 
 
 }
